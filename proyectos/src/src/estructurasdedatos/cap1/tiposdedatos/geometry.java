ea0f8769de267a487ba317fdd83885df59d5c991
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-08-28 23:40:05 -0500

    Create geometry.java

---
+++ proyectos/src/src/estructurasdedatos/cap1/tiposdedatos/geometry.java
@@ -0,0 +1,40 @@
+public class Figura {
+    
+    private String codigo;
+    private String tipo;
+    private String nombre;
+ 
+    
+
+    
+  
+    public String getCodigo() {
+        return codigo;
+    }
+  
+   
+    public void setCodigo(String Codigo) {
+        this.codigo = codigo;
+    }
+    
+   
+    public String getTipo() {
+        return tipo;
+    }
+   
+   
+    public void setTipo(String Tipo) {
+        this.tipo = tipo;
+    }
+  
+   
+    public String getNombre() {
+        return nombre;
+    }
+    
+   
+    public void setNombre(String Nombre) {
+        this.nombre = nombre;
+    }
+    
+} 

8b058e7ce4d7fa4df13e2110d91ce9e17f9eb59a
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-09-10 15:16:09 -0500

    Update Vector.java

--- proyectos/src/src/estructurasdedatos/cap2/arreglos/Vector.java
+++ proyectos/src/src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -8,50 +8,77 @@ package estructuraDeDatos.estructuraVector;
 /*@author Diego Mauricio Galvis Arboleda.*/
 
 public class Vector {
+private int tamaño;
+private String[]vector;
 
-    private int tam;
-    private String[] vector;
+public void crearVector(int d){
+this.tamaño=d;
+this.vector=new String[this.tamaño];
+}
 
-    public void crearVector(int n) {
-        this.tam = n; 
-        this.vector = new String[this.tam];
-    }
-    public void setTam(int t) {
-        this.tam = t;
-    }
-    public int gettam() {
-        return tam;
-    }
-    public boolean Elimiar(int t) {  
-        this.vector = null;
-        return true;
-    }
-    public void Insertar(String n, int pos) { 
-        vector[pos] = n;                      
-    }
-    public String[] listar() {
-        for (int i = 0; i < tam; i++) {      
-            System.out.println(vector[i]);
-        }
-        return vector;
-    }
-    public void buscarDato(String dato) {
-        int i = 0;
-        while (i < tam) {                    
-            if (vector[i].equals(dato)) {    
-                System.out.println("El Dato esta en la posicion: " + i);
+public void settamaño(int s){
+this.tamaño=s;
+}
+public int gettamaño(){
+return tamaño;
+}
+public boolean eliminar(int m){
+this.vector=null;
+return true;
+}
+public void insertar(String n, int pos){
+this.vector[pos]=d;
+}
+
+public String[] listar(){
+for (int l=0;l<=tamaño; l++){
+System.out.println(vector[l]);
+}
+
+
+public void ordenarVec(int vector[]){
+for(int i = 0; i < vector.length - 1; i++)
+        {
+            for(int j = 0; j < vector.length - 1; j++)
+            {
+                if (vector[j] < vector[j + 1])
+                {
+                    int tmp = vector[j+1];
+                    vector[j+1] = vector[j];
+                    vector[j] = tmp;
+                }
             }
-            i++;
         }
-    }
-    public void actualizar (int pos, String dato){   
-        if(pos < tam){                             
-            System.out.println("La posicion no existe"); 
-        }else {
-            vector[pos]= dato; 
-        }
-    }
-    public void borrarposicion (int pos){             
-        vector[pos]= null;                           
-    }
+    
+}
+
+public void buscarBinario(int pos){
+int posini=0;
+int posfin=vector.length-1;
+while(posin0 i<=posfin)
+poscentral = (posfin+posini)/2;
+if (vector[poscentral]==pos){
+System.out.println("el dato esta en:"+posini+"y"+posfin);
+}
+else {if(pos<=vector[poscentral]){
+posfin=poscentral;
+}
+}
+}
+}
+public void actualizar(int pos, int d){
+if ((pos<tamaño)&&(pos>0)){
+this.vector[pos]=d;
+}
+else{
+System.out.println("tamaño del vector incorrecto");
+}
+}
+public void borrar(int pos){
+if ((pos<tamaño)&&(pos<0))
+    this.vector[pos]=null;
+}
+else {
+System.out.println("tamaño incorrecto");
+}
 }

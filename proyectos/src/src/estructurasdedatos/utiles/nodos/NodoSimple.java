fd34227feed7e07e5c4d59b326f1f7e805d216a9
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-09-17 13:36:46 -0500

    Update NodoSimple.java

--- proyectos/src/src/estructurasdedatos/utiles/nodos/NodoSimple.java
+++ proyectos/src/src/estructurasdedatos/utiles/nodos/NodoSimple.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package estructurasdedatos.utiles.nodos;
+package nodos;
 
-/**
- *
- * @author tusk
- */
-public class NodoSimple {
-    
+/*@Diego Mauricio Galvis Arboleda*/
+public class Nodo {
+
+    private Nodo siguiente;
+    private int dato;
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public void crearnodo() {
+        this.siguiente = null;
+        this.dato = 0;
+
+    }
 }

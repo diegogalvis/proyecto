a5ad1d3de572d2e12369c14cd90c6154730e6424
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-10-06 18:12:59 -0500

    Update Diccionario.java

--- proyectos/src/src/estructurasdedatos/cap3/mapas/Diccionario.java
+++ proyectos/src/src/estructurasdedatos/cap3/mapas/Diccionario.java
@@ -1,14 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurasdedatos.cap3.mapas;
 
-/**
- *
- * @author tusk
- */
+import estructurasdedatos.utiles.nodos.NodoImpl;
+
+/*@author Diego Mauricio Galvis Arboleda*/
 public class Diccionario {
+
+    NodoImpl cabeza;
+    NodoImpl cola;
+
+    public Diccionario() {
+        cabeza = null;
+        cola = null;
+    }
+
     
+    public void ingresarpaldicc(String paldicc, String resultado) {
+        NodoImpl nuevapaldicc = new NodoImpl();
+        nuevapaldicc.setpaldicc(paldicc);
+        nuevapaldicc.setresultado(resultado);
+
+        if (cabeza == null) {
+            cabeza = nuevapaldicc;
+            cabeza.setresultado(null);
+            cola = cabeza;
+
+        } else {
+            cola.setsiguiente(nuevapaldicc);
+            nuevapaldicc.setresultado(null);
+            cola = nuevapaldicc;
+        }
+    }
+
+    
+    
+    public void verpaldicc() {
+        NodoImpl palactual = new NodoImpl();
+        palactual = cabeza;
+        while (palactual != null) {
+            System.out.println(palactual.getpaldicc() + " " + palactual.getresultado());
+            palactual = palactual.getsiguiente();
+        }
+        System.out.println("");
+    }
+
+    
+    public void modificarpaldicc(String palabra, String nuevoresultado) {
+        NodoImpl palactual  = new NodoImpl();
+       palactual  = cabeza;
+        int estado = 0;
+        while (palactual  != null) {
+            if (palactual .getpaldicc().equals(palabra) == true) {
+
+                palactual .setresultado(nuevoresultado);
+                estado = 1;
+
+            }
+            palactual  = palactual .getsiguiente();
+        }
+        if (estado == 0) {
+            System.out.println("No es del diccionario");
+        }
+    }
+
+    
+    
+    public void borrarpaldicc(String palabra) {
+        NodoImpl palactual = new NodoImpl();
+        NodoImpl anterior = new NodoImpl();
+         palactual = cabeza;
+        anterior = null;
+        int estado = 0;
+        while (palactual!= null) {
+            if (palactual.getpaldicc().equals(palabra) == true) {
+                estado = 1;
+                if (palactual == cabeza) {
+                    cabeza = cabeza.getsiguiente();
+                } else {
+                    anterior.setsiguiente(palactual.getsiguiente());
+          
+                
+                }
+
+            }
+            anterior = palactual;
+           palactual = palactual.getsiguiente();
+        }
+        if (estado == 0) {
+            System.out.println("No es del diccionario");
+        }
+    }
+
 }

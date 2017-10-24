e4b7ebf1821cd0c55f0bb9cf272bc3580b097492
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-10-24 12:19:31 -0500

    Update TablaHash.java

--- proyectos/src/src/estructurasdedatos/cap3/mapas/TablaHash.java
+++ proyectos/src/src/estructurasdedatos/cap3/mapas/TablaHash.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package estructurasdedatos.cap3.mapas;
+package estructuraDeDatos.estructuraDiccionario;
 
-/**
- *
- * @author tusk
- */
+/* @ autor Diego Mauricio Galvis A */
+
 public class TablaHash {
-    
+
+    private Map[] tablaHash;
+    private int tamaño;
+
+    public boolean crearTabla(int t) {
+
+        this.tamaño = t;
+        boolean salida = true;
+        if (this.tablaHash == null) {
+            this.tablaHash = new Map[this.tamaño];
+            System.out.print(" Se creo \n");
+        } else {
+            System.out.print("No se creo \n");
+            salida = false;
+        }
+        return salida;
+    }
+
+    public boolean insertarDato(int llave, String valor) {
+
+        Map nuevo = new Map(llave, valor);
+
+        int pos = this.funcionHash(llave);
+        this.tablaHash[pos] = nuevo;
+        return true;
+    }
+
+    public int funcionHash(int llave) {
+        int pos = llave % this.tamaño;
+        if (this.tablaHash[pos] == null) {
+            return pos;
+        } else {
+            while (this.tablaHash[pos] != null) {
+                if (pos < tamaño) {
+                    pos = pos++;
+                } else {
+                    pos = 0;
+                }
+            }
+        }
+        return pos;
+    }
+
+    public Map buscarDato(int llave) {
+        int pos = this.funcionHash(llave);
+        return this.tablaHash[pos];
+    }
+
+    public void eliminarPos(int pos) {
+        if (pos > tamaño) {
+            System.out.println(" No existe la posicion \n");
+        } else {
+            this.tablaHash[pos] = null;
+        }
+    }
 }

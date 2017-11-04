391414f9540544f6387cb2e19b1f5367a4dd6408
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-11-03 21:05:14 -0500

    Create Vertice.java

---
+++ proyectos/src/src/estructurasdedatos/cap5/grafos/Vertice.java
@@ -0,0 +1,24 @@
+/* * To change this license header, choose License Headers in Project Properties. 
+* To change this template file, choose Tools | Templates 
+* and open the template in the editor. 
+*/
+
+package estructurasdedatos.cap5.grafos;
+
+/** * * @author Diego Mauricio Galvis */
+
+public class Vertice {    
+  
+    private String dato;
+    
+public String getDato() {
+      return dato; 
+      
+      }
+      
+    public void setDato(String dato) { 
+        this.dato = dato;
+        
+        
+        }
+        }

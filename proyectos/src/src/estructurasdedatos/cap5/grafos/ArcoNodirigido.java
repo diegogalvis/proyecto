7f0a89314653c2aaf408f583fb93391eebbe27f4
diegogalvis <30878298+diegogalvis@users.noreply.github.com>
2017-11-03 21:02:51 -0500

    Create ArcoNodirigido.java

---
+++ proyectos/src/src/estructurasdedatos/cap5/grafos/ArcoNodirigido.java
@@ -0,0 +1,31 @@
+/* 
+* To change this license header, choose License Headers in Project Properties. 
+* To change this template file, choose Tools | Templates 
+* and open the template in the editor. 
+*/
+
+package estructurasdedatos.cap5.grafos;
+
+/*** * @author Diego Mauricio Galvis 
+*/    
+public class ArcoNoDirigido {       
+  private Vertice v1;        
+  private Vertice v2;          
+  public Vertice getV1() {           
+    return v1;       
+} 
+
+public void setV1(Vertice v1) {   
+  this.v1 = v1;      
+
+}       
+
+public Vertice getV2() {   
+  return v2;          
+} 
+
+public void setV2(Vertice v2) {  
+  this.v2 = v2;      
+
+}     
+}
